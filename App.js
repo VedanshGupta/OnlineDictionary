fbb90289dfb454cb2d05080eea935132ce9b1245
VedanshGupta <68778187+VedanshGupta@users.noreply.github.com>
2021-03-24 17:15:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Component }from 'react';
 import { View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default class App extends Component{
 	render(){ 
 	  return (
-	    <View>
-	      <HomeScreen />
-	    </View>
+      <SafeAreaProvider>
+        <View>
+          <HomeScreen />
+        </View>
+      </SafeAreaProvider>
 	  )
 	}
 }
